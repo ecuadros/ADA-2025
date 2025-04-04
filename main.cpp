456965ca41845f3f169e581467cdbbcedea73771
ecuadros <ecuadros@spc.org.pe>
2025-04-03 19:34:03 -0500

    suma verificada

--- main.cpp
+++ main.cpp
@@ -7,5 +7,8 @@ int suma(int a, int b){
 
 // clang++ -o main main.cpp
 void main(){
-    cout << "Hello world!" << endl
+    cout << "Hello world!" << endl;
+    int x = 5, y = 10, z;
+    z = suma(x, y);
+    cout << "z=" << z << endl;
 }
