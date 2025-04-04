3ee7d9ed0cb91c9b023d54c3cefc8d76ab0968b3
ecuadros <ecuadros@spc.org.pe>
2025-04-03 19:37:57 -0500

    mult

--- main.cpp
+++ main.cpp
@@ -5,10 +5,16 @@ int suma(int a, int b){
     return a+b;
 }
 
+int mult(int a, int b){
+    return a*b;
+}
 // clang++ -o main main.cpp
 void main(){
     cout << "Hello world!" << endl;
     int x = 5, y = 10, z;
     z = suma(x, y);
-    cout << "z=" << z << endl;
+    cout << "suma z=" << z << endl;
+
+    z = mult(x, y);
+    cout << "mult z=" << z << endl;
 }
