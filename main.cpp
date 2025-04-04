d597d7ba47c40c58ba7f020a3e547ed88a01f549
ecuadros <ecuadros@spc.org.pe>
2025-04-03 19:42:27 -0500

    esta lista parta crearse

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int resta(int a, int b){
+    return a-b
+}
+
 // clang++ -o main main.cpp
 void main(){
     cout << "Hello world!" << endl
 
-    int resta(int a, int b){
-        return a-b
-    }
+    // Resta va aqui
 }
